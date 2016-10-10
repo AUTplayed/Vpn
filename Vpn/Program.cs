a95f842589ef5b456668ff9bce6a02759fc3ce8c
Philipp Hemmelmayr <fips.hem@gmail.com>
2016-10-10 09:40:39 +0200

    Finishing console vpn and adding fancy commends. Also letting codemaid tidy up autogenerated code

--- Vpn/Program.cs
+++ Vpn/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Windows.Automation;
 
 namespace Vpn
 {
@@ -12,28 +11,34 @@ namespace Vpn
         [STAThread]
         private static void Main(string[] args)
         {
-            //Getting the Password
+            //Getting the Password via ugly splits
             string html = new WebClient().DownloadString("https://www.vpnme.me/freevpn.html");
             string split = html.Split(new[] { "features-table2" }, StringSplitOptions.None)[1];
             split = split.Split(new[] { "<tbody>" }, StringSplitOptions.None)[1];
             string[] td = split.Split(new[] { "<td>" }, StringSplitOptions.None);
             string pw = td[7].Split('<')[0];
             Console.WriteLine(pw);
-            System.Windows.Forms.Clipboard.SetText(pw);
+            Console.WriteLine("Initiating VPN...");
 
             //Opening VPN tunnel
             String pwPath = @"C:\vpn_pw.txt";
             String configPath = "\"" + @"C:\Program Files\OpenVPN\config\vpnme_fr_tcp443.ovpn" + "\"";
 
+            //Creating a File with user credentials
             if (File.Exists(pwPath))
                 File.Delete(pwPath);
-            File.Create(pwPath);
+            File.Create(pwPath).Close();
             File.WriteAllLines(pwPath, new List<string> { "fr-open", pw });
-            string cmd = "openvpn.exe --config " + configPath + " --auth-user-pass " + pwPath;
-            Process.Start("CMD.exe", cmd);
-            File.Delete(pwPath);
 
-            Console.ReadKey();
+            //Creating console statements
+            string vpn = "openvpn.exe --config " + configPath + " --auth-user-pass " + pwPath;
+            string color = "color a";
+            string cmd = "/C " + color + " & " + vpn;
+
+            //Starting comands
+            Process.Start("CMD.exe", cmd);
+            System.Threading.Thread.Sleep(2000);
+            File.Delete(pwPath);
         }
     }
 }
